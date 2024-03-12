9d1b5feec7ceb17481622f7dc46992992f82e22d
Dragos Andreita <dragosaandreita@gmail.com>
2024-03-12 13:41:23 +0200

    Your commit message here

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,8 +4,8 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
-//const socket = io('https://chat-with-me-now-af5ae915cbad.herokuapp.com/');
+//const socket = io('http://localhost:5000');
+const socket = io('https://chat-with-me-now-fa6155f098c2.herokuapp.com/');
 
 const ContextProvider = ({ children }) => {
     const [callAccepted, setCallAccepted] = useState(false);
